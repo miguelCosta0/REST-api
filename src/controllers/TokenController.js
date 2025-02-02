8ebfc27e8e9302423e092f009c8be1f1c9f3cb40
Miguel Costa <mapcosta999@gmail.com>
2025-02-02 13:19:10 -0300

    alterando a resposta de TokenController

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -2,35 +2,37 @@ import User from '../models/User.js'
 import jwt from 'jsonwebtoken';
 
 class TokenController {
-    async store(req, res) {
-        const {email = '', password = ''} = req.body;
+  async store(req, res) {
+    const { email = '', password = '' } = req.body;
 
-        if (!email || !password)
-            return res.status(400).json({
-                errors: ['credenciais inválidas']
-            });
+    if (!email || !password)
+      return res.status(400).json({
+        errors: ['credenciais inválidas']
+      });
 
-        const user = await User.findOne({
-            where: {
-                email: email
-            }
-        })
-        
-        if (!user)
-            return res.status(400).json({
-                errors: ['Usuário inexistente']
-            });
-        
-        if (!user.passwordIsValid(password))
-            return res.status(400).json({
-                errors: ['Senha inválida']
-            });
-        
-        const { id } = user;
-        return res.json(jwt.sign({id, email}, process.env.TOKEN_SECRET, {
-            expiresIn: process.env.TOKEN_EXPIRATION
-        }));
-    }
+    const user = await User.findOne({
+      where: {
+        email: email
+      }
+    })
+
+    if (!user)
+      return res.status(400).json({
+        errors: ['Usuário inexistente']
+      });
+
+    if (!user.passwordIsValid(password))
+      return res.status(400).json({
+        errors: ['Senha inválida']
+      });
+
+    const { id } = user;
+    const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION
+    });
+
+    return res.json({ token, user: {name: user.nome, id, email} });
+  }
 }
 
 export default new TokenController;
