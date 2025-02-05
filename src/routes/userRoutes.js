5ef0ba3952332bb3de376a8cc3c56f805ccc6501
Miguel Costa <mapcosta999@gmail.com>
2025-02-05 15:25:27 -0300

    retirando loginRequired para criar usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,12 +5,12 @@ import loginRequired from '../middlewares/loginRequired.js';
 
 const router = new Router();
 
-router.get('/',UserController.index); // Não existe em uma aplicação real
-router.get('/', UserController.show); // Não existe em uma aplicação real
+//router.get('/',UserController.index); // Não existe em uma aplicação real
+//router.get('/', UserController.show); // Não existe em uma aplicação real
 
 
-router.post('/', loginRequired, UserController.create);
+router.post('/', UserController.create);
 router.patch('/', loginRequired, UserController.update);
-router.delete('/',  loginRequired, UserController.delete);
+router.delete('/', loginRequired, UserController.delete);
 
 export default router;
